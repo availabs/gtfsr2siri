91a9af2f8d9ab7d114d0e758637c6fd38c15b6f9
Paul Tomchik <pauljtomchik@gmail.com>
2015-07-20 10:28:49 -0400

    Refactoring.

--- node-protobuf/bus-vehicle-monitoring-feed-reader.js
+++ node-protobuf/bus-vehicle-monitoring-feed-reader.js
@@ -2,23 +2,6 @@
 
 'use strict';
 
-var http     = require("https"),
-    key      = require('./BUS-API-KEY');
+var feedReader = require('./bus-feed-reader');
 
-var feedUrl = 'https://bustime.mta.info/api/siri/vehicle-monitoring.json?key=' + key + '&OperatorRef=MTA&MonitoringRef=308209&LineRef=MTA%20NYCT_B63';
-
-http.get(feedUrl, parse);
-
-function parse(res) {
-    var data = '';
-
-    res.on("data", function(chunk) {
-        data += chunk;
-    });
-
-    res.on("end", function() {
-        var msg = JSON.parse(data);
-
-        console.log(JSON.stringify(msg, null, 4));
-    }); 
-};
+feedReader('vehicle');
